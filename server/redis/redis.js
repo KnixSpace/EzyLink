3a646c33e500fefb19c01c722ea04776cda010c1
INFINX <krupalgp2003@gmail.com>
2023-09-28 18:41:24 +0530

    redis env variable

--- server/redis/redis.js
+++ server/redis/redis.js
@@ -1,14 +1,16 @@
-const { Redis } = require("ioredis");
-// const { Redis } = require("@upstash/redis");
-// const redis = new Redis({
-//   url: process.env.UPSTASH_REDIS_REST_URL,
-//   token: process.env.UPSTASH_REDIS_REST_TOKEN,
-// });
+require("dotenv").config();
+const { Redis } = require("@upstash/redis");
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
-const redis = new Redis();
+// const { Redis } = require("ioredis");
+// const redis = new Redis();
+
 const initialSet = async () => {
   const count = await redis.get("counter");
-  console.log(count);
+  // console.log(count);
   if (!count) {
     console.log("initial counter set");
     await redis.set("counter", 1);
